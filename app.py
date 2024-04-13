08f648d3c9265f088c9f8f7b084337e999fa6772
Saurabh Shinde <shindesd27797@gmail.com>
2024-04-13 15:18:21 +0530

    first commit

--- app.py
+++ app.py
@@ -1,23 +1,41 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from fastapi import FastAPI
-import base64
 from pydantic import BaseModel
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     AIMessage,
     HumanMessage,
     SystemMessage)
-import json
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
+import os
+from fastapi.middleware.cors import CORSMiddleware
+import logging
+
+logging.basicConfig( level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
+# openai_api_key = os.getenv('OPENAI_API_KEY')
+openai_api_key="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"
 
-openai_api_key='YOUR_OPENAI_API_KEY'
+
+app = FastAPI()
+
+# Add middleware to enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can set specific origins here instead of "*"
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 tokens=300
 gpt = ChatOpenAI(max_tokens=tokens,api_key=openai_api_key)
 
-app = FastAPI()
+# app = FastAPI()
 
 class TextData(BaseModel):
     text: str
@@ -25,22 +43,19 @@ class TextData(BaseModel):
 
 @app.post("/assiatant")
 async def talk(text_data: TextData):
-    embeddings = OpenAIEmbeddings(api_key=decoded)
+    embeddings = OpenAIEmbeddings(api_key=openai_api_key)
     db3 = Chroma(persist_directory='./chroma',collection_name='my_details', embedding_function=embeddings)
     docs = db3.similarity_search(text_data.text,k=4)
     content=''.join([i.page_content for i in docs])
-
     initial_prompt=f"""
     Your name is surbhi and you are an AI assistant of saurabh and you know only the below details.
-    Introduce yourself as an A.I. assiatant of saurabh. You and saurabh are different persons.
+    Introduce yourself as an AI assiatant of saurabh. You and saurabh are different persons.
     Anything other than the below content, your name and your role should not be answered.
     
     Contet: {content} 
     
     Question:{text_data.text}
     
-    Your name is surbhi and you are an A.I. assistant of saurabh
-    Give precise and short answers. Answer in a way yor are telling about saurabh. 
     If the user want more information about saurabh tell like please check out his resume.
     And if the question is not related to the above content tell like you know information only about saurabh.
     """
